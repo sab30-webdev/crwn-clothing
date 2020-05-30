b723de17d73dd57a1bdc9046213ac82f2f69749f
sab30-webdev <sabarinath3072001@gmail.com>
2020-05-30 18:42:17 +0530

    created react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import HomePage from './homepage.component'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <HomePage/>;
+  }
+}
+
+export default App;
