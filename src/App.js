759ee9819b525ecd4a161735eb50127e3f012e73
sab30-webdev <sabarinath3072001@gmail.com>
2020-06-02 19:55:04 +0530

    added shop and header component and set react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
-import HomePage from './pages/homepage/homepage.component'
-import './App.css';
+import React, { Component } from "react";
+import HomePage from "./pages/homepage/homepage.component";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component.jsx";
 
 class App extends Component {
   render() {
-    return <HomePage/>;
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+        </Switch>
+      </div>
+    );
   }
 }
 
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+
+import Directory from "../../components/directory/directory.component";
+
 import "./homepage.styles.scss";
-import Directory from "../../components/directory/directory.component.jsx";
 
 const HomePage = () => (
   <div className="homepage">
-    <h1>Welcome to my Homepage</h1>
     <Directory />
   </div>
 );
